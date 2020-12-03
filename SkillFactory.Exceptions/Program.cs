08bc89780ac0c9b299cb99f78991390a88a16ebc
Дмитрий Афанасьев <a1d1@inbox.ru>
2020-12-03 21:42:18 +0800

    Исключения

--- SkillFactory.Exceptions/Program.cs
+++ SkillFactory.Exceptions/Program.cs
@@ -7,47 +7,47 @@ namespace SkillFactory.Exceptions
     {
         static void Main(string[] args)
         {
-            //var firstParam = args.FirstOrDefault();
+            var firstParam = args.FirstOrDefault();
 
-            //if (firstParam == null)
+            if (firstParam == null)
+            {
+                throw new ArgumentNullException("Укажите обязательно первый аргумент для процесса расчета");
+            }
+
+            //try
             //{
-            //    throw new ArgumentNullException("Укажите обязательно первый аргумент для процесса расчета");
+            //    Console.WriteLine("Введите первое число ");
+            //    var number1 = int.Parse(Console.ReadLine());
+
+            //    Console.WriteLine("Введите второе число ");
+            //    var number2 = int.Parse(Console.ReadLine());
+
+            //    var total = number1 / number2;
+
+            //    //throw new MyException("Выбросим свое исключение");
+            //    throw new StackOverflowException();
             //}
+            //catch (DivideByZeroException dividEx)
+            //{
+            //    Console.WriteLine($"Блок DivideByZeroException messsage { dividEx.Message }");
+            //    Console.WriteLine($"Блок DivideByZeroException stack { dividEx }");
+            //}
+            //catch (MyException myException)
+            //{
+            //    Console.WriteLine($"Блок MyException messsage { myException.Message }");
+            //    Console.WriteLine($"Блок MyException stack { myException }");
 
-            try
-            {
-                Console.WriteLine("Введите первое число ");
-                var number1 = int.Parse(Console.ReadLine());
-
-                Console.WriteLine("Введите второе число ");
-                var number2 = int.Parse(Console.ReadLine());
-
-                var total = number1 / number2;
-
-                //throw new MyException("Выбросим свое исключение");
-                throw new StackOverflowException();
-            }
-            catch (DivideByZeroException dividEx)
-            {
-                Console.WriteLine($"Блок DivideByZeroException messsage { dividEx.Message }");
-                Console.WriteLine($"Блок DivideByZeroException stack { dividEx }");
-            }
-            catch (MyException myException)
-            {
-                Console.WriteLine($"Блок MyException messsage { myException.Message }");
-                Console.WriteLine($"Блок MyException stack { myException }");
-
-                throw new Exception("Выбросим опять исключение т.к программа не должна уже работать!");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Блок общий Exception messsage { ex.Message }");
-                Console.WriteLine($"Блок общий Exception stack { ex }");
-            }
-            finally
-            {
-                Console.WriteLine("Блок finally освободим ресурсы которые использовали");
-            }
+            //    throw new Exception("Выбросим опять исключение т.к программа не должна уже работать!");
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine($"Блок общий Exception messsage { ex.Message }");
+            //    Console.WriteLine($"Блок общий Exception stack { ex }");
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Блок finally освободим ресурсы которые использовали");
+            //}
 
             Console.ReadLine();
         }
